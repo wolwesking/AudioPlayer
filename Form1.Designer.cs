41b16397ad9707ea7aad5f5fe1cab8e8503bb8b5
wolwesking <83821890+wolwesking@users.noreply.github.com>
2022-07-10 12:56:35 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MusicPlayerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BT_yt = new System.Windows.Forms.Button();
+            this.BT_pc = new System.Windows.Forms.Button();
+            this.VideoPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.PlayList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BT_yt
+            // 
+            this.BT_yt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_yt.Location = new System.Drawing.Point(670, 362);
+            this.BT_yt.Name = "BT_yt";
+            this.BT_yt.Size = new System.Drawing.Size(118, 76);
+            this.BT_yt.TabIndex = 1;
+            this.BT_yt.Text = "Select music from youtube";
+            this.BT_yt.UseVisualStyleBackColor = true;
+            // 
+            // BT_pc
+            // 
+            this.BT_pc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BT_pc.Location = new System.Drawing.Point(546, 362);
+            this.BT_pc.Name = "BT_pc";
+            this.BT_pc.Size = new System.Drawing.Size(118, 76);
+            this.BT_pc.TabIndex = 1;
+            this.BT_pc.Text = "Selenc music from pc";
+            this.BT_pc.UseVisualStyleBackColor = true;
+            this.BT_pc.Click += new System.EventHandler(this.BT_pc_Click);
+            // 
+            // VideoPlayer
+            // 
+            this.VideoPlayer.Enabled = true;
+            this.VideoPlayer.Location = new System.Drawing.Point(12, 13);
+            this.VideoPlayer.Name = "VideoPlayer";
+            this.VideoPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("VideoPlayer.OcxState")));
+            this.VideoPlayer.Size = new System.Drawing.Size(530, 425);
+            this.VideoPlayer.TabIndex = 2;
+            this.VideoPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.PlayStateChange);
+            // 
+            // PlayList
+            // 
+            this.PlayList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayList.FormattingEnabled = true;
+            this.PlayList.Location = new System.Drawing.Point(548, 13);
+            this.PlayList.Name = "PlayList";
+            this.PlayList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.PlayList.Size = new System.Drawing.Size(240, 342);
+            this.PlayList.TabIndex = 3;
+            this.PlayList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(388, 408);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PlayList);
+            this.Controls.Add(this.VideoPlayer);
+            this.Controls.Add(this.BT_pc);
+            this.Controls.Add(this.BT_yt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.VideoPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BT_yt;
+        private System.Windows.Forms.Button BT_pc;
+        private AxWMPLib.AxWindowsMediaPlayer VideoPlayer;
+        private System.Windows.Forms.ListBox PlayList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Global variables
+        String[] paths = { };
+        String[] fileN = { };
+
+        //Playlist
+        int selectedFile = 0;
+
+        //FileSystem
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void BT_pc_Click(object sender, EventArgs e)
+        {
+            VideoPlayer.Ctlcontrols.stop();
+
+            if (fileN.Length > 1)
+            {
+                fileN = null;
+
+                PlayList.Items.Clear();
+
+                selectedFile = 0;
+                MessageBox.Show("Your playlist is cleard");
+            }
+            //Settings
+            ofd.Filter = "Audio (*.mp3,*.acc,*.wma,*.m4a,*.avi,*.aif,*.aifc,*.aiff,*.wav,*.cda,*.adt,*.adts)|*.mp3;*.acc;*.wma;*.m4a;*.avi;*.aif;*.aifc;*.aiff;*.wav;*.cda;*.adt;*.adts|All Files (*.*)|*.*";
+            ofd.Multiselect = true;
+
+            //Open dialog
+            DialogResult result = ofd.ShowDialog();
+
+            //Check if its opened
+            if (result == DialogResult.OK)
+            {
+                // Get data
+                fileN = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+
+                LoadPlayList();
+            }
+
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedFile = PlayList.SelectedIndex;
+            PlayFile(PlayList.SelectedItem.ToString());
+
+        }
+
+        private void PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+
+        }
+
+        private void LoadPlayList()
+        {
+            VideoPlayer.currentPlaylist = VideoPlayer.newPlaylist("Playlist", "");
+
+            foreach (string audio in paths)
+            {
+                VideoPlayer.currentPlaylist.appendItem(VideoPlayer.newMedia(audio));
+                PlayList.Items.Add(audio);
+            }
+
+            if (paths.Length > 0)
+            {
+                PlayList.SelectedIndex = selectedFile;
+                PlayFile(PlayList.SelectedItem.ToString());
+            }
+        }
+
+        private void PlayFile(string url)
+        {
+            VideoPlayer.URL = url;
+        }
+    }
+}
